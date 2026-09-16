9c1b534d6fa6c084814328dab151fbf3fc8b3309
MatheusRetroz <matheusretrozs@gmail.com>
2026-09-16 10:08:03 -0300

    Loja Online: Fix - Alterei a logica da conta do valor

--- loja-online/app.js
+++ loja-online/app.js
@@ -2,7 +2,7 @@ function calcularTotal (itens) {
     let total = 0
 
     for (let i = 0; i < itens.length; i++){
-        total += itens[i].valor * 1.1
+        total += itens[i].valor
     }
 
     return total
